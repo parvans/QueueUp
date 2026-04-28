ccb71fa28e2ecd425676cb57b9b754df6d91a005
parvans <parvansajeevan@gmail.com>
2026-04-28 23:11:01 +0530

    add basic file structure

---
+++ app/queue/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const QueueDetails = () => {
+  return (
+    <View>
+      <Text>QueueDetails</Text>
+    </View>
+  )
+}
+
+export default QueueDetails
---
+++ app/queue/ticket/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TicketDetails = () => {
+  return (
+    <View>
+      <Text>TicketDetails</Text>
+    </View>
+  )
+}
+
+export default TicketDetails
