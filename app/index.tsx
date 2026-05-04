9e5e76c2af4708f6b48cb8b2bcdb65e8b8320ccb
parvans <parvansajeevan@gmail.com>
2026-05-04 19:54:56 +0530

    updated the root index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,29 @@
 // app/index.tsx
+import { Colors } from '@/constants/colors';
+import { useAuthStorage } from '@/store/authStore';
 import { Redirect } from 'expo-router';
+import { useEffect } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function Index() {
   // This is where you'll later check AsyncStorage for an auth token.
   // If token exists → go to tabs. If not → show welcome.
   // For now we always redirect to welcome.
-  return <Redirect href="/(auth)/welcome" />;
+
+  const {user, loadFromStorage, isLoading} = useAuthStorage();
+
+  useEffect(()=>{
+    loadFromStorage();  // check AsyncStorage for saved session on app launch
+  },[]);
+
+  if(isLoading){
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator color={Colors.primary} size="large" />
+      </View>
+    )
+  }
+  return user
+    ? <Redirect href="/(tabs)" />
+    : <Redirect href="/(auth)/welcome" />;
 }
