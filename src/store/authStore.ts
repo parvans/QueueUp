5c323e195f6bcb0a697ed44969c2ce22b91f18f7
parvans <parvansajeevan@gmail.com>
2026-05-04 19:44:43 +0530

    added the zustand authStore

---
+++ src/store/authStore.ts
@@ -0,0 +1,75 @@
+import { create } from 'zustand';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { loginAPI, registerAPI } from '@/api/auth.api';
+
+type User = {
+  id: string; 
+  name: string; 
+  email: string; 
+  role: string;
+};
+
+type AuthStore = {
+    user: User | null;
+    accessToken: string | null;
+    isLoading: boolean;
+    error: string | null;
+
+    login:(email:string, password:string)=> Promise<void>;
+    register:(name:string, email:string, password:string)=> Promise<void>;
+    logout:()=> Promise<void>;
+    loadFromStorage:()=> Promise<void>;
+    clearError:()=> void;
+};
+
+export const useAuthStorage = create<AuthStore>((set)=>({
+    user: null,
+    accessToken: null,
+    isLoading: false,
+    error: null,
+
+    login: async(email, password)=>{
+        set({isLoading:true, error:null});
+
+        try {
+            const { user, accessToken, refreshToken } = await loginAPI(email, password);
+            await AsyncStorage.multiSet([
+                ['accessToken', accessToken],
+                ['refreshToken', refreshToken],
+                ['user', JSON.stringify(user)],
+            ]);
+            set({user, accessToken, isLoading:true});
+
+        } catch (e:any) {
+            set({ error: e.response?.data?.error || 'Login failed', isLoading: false });
+        }
+    },
+    register: async(name, email, password)=>{
+        set({isLoading:true, error:null});
+        try {
+            const { user, accessToken, refreshToken } = await registerAPI({ name, email, password });
+            await AsyncStorage.multiSet([
+                ['accessToken',  accessToken],
+                ['refreshToken', refreshToken],
+                ['user',         JSON.stringify(user)],
+            ]);
+            set({ user, accessToken, isLoading: false });
+        } catch (e:any) {
+            set({ error: e.response?.data?.error || 'Registration failed', isLoading: false });
+        }
+    },
+    logout: async () => {
+        await AsyncStorage.multiRemove(['accessToken', 'refreshToken', 'user']);
+        set({ user: null, accessToken: null });
+    },
+
+  loadFromStorage: async () => {
+    const [[, token], [, userStr]] = await AsyncStorage.multiGet([
+      'accessToken', 'user',
+    ]);
+    if (token && userStr) {
+      set({ accessToken: token, user: JSON.parse(userStr) });
+    }
+  },
+    clearError: () => set({ error: null }),
+}))
