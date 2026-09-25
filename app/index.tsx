152c76a5619ca6453ae1d37cbe3bf0d727b1328e
parvans <parvansajeevan@gmail.com>
2026-09-25 15:18:02 +0530

    Start from scratch again, app setup and native wind added

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-2xl font-bold text-white">
+        NativeWind works! 🎉
+      </Text>
+    </View>
+  );
+}
